66a4353d407425433851f1ecacdb0b26d2695dd3
Araceli Cabana <aracelicabana94@gmail.com>
2023-03-20 18:45:34 -0300

    primer commit

---
+++ algoritmo.js
@@ -0,0 +1,10 @@
+document.querySelector("body").innerHTML = `
+
+<h1>Titulo 1</h1>
+    <h2>Titutlo 2</h2>
+    <h3>titulo 3</h3>
+
+    <img src="https://cdn0.recetasgratis.net/es/posts/8/7/4/torta_de_cumpleanos_para_diabeticos_62478_600.webp"
+        alt=" torta de cumple">
+    <img src="imagenes/tortaa.webp" alt="torta1">
+`;
